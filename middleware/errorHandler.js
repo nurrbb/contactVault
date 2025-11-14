2cab1562519b6c22d113517338c5bb62737fa899
nurrbb <enurb320@gmail.com>
2025-11-14 11:49:26 +0300

    Add error handler middleware and update contact controller

---
+++ middleware/errorHandler.js
@@ -0,0 +1,26 @@
+const{constants} = require("../constants")
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+             res.json({title: "Validation Failed", message: err.message, stackTrace: err.stackTrace});
+            break;
+        case constants.NOT_FOUND:
+          res.json({title: "Not found", message: err.message, stackTrace: err.stackTrace});
+            break;
+        case constants.UNAUTHORIZED:
+             res.json({title: "Unauthotized", message: err.message, stackTrace: err.stackTrace});
+            break;
+        case constants.FORBIDDEN:
+          res.json({title: "Forbidden", message: err.message, stackTrace: err.stackTrace});
+            break;
+        case constants.SERVER_ERROR:
+          res.json({title: "Server Error", message: err.message, stackTrace: err.stackTrace});
+            break;
+        default:
+            console.log("No error, All good!")
+            break;
+    }
+};
+
+module.exports = errorHandler;
